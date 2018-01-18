261f1bd6ae74305b208840068d2c075a097f6602
adusa1019 <help.me.adusa@gmail.com>
2018-01-18 20:59:57 +0900

    temporary changes

--- utils/tmp.py
+++ utils/tmp.py
@@ -4,38 +4,66 @@
 import argparse
 import glob
 import os
-import random
 import sys
+import random
 
-import numpy as np
 import chainer
-from PIL import Image
+import numpy as np
 
 
 class SamplingDataset(chainer.dataset.DatasetMixin):
 
-    def __init__(self, dataset, num_class, num_per_class, num_sample_class, num_sample_per_class):
+    def __init__(self,
+                 dataset,
+                 num_class,
+                 num_data_per_class,
+                 num_sample_class,
+                 num_sample_per_class,
+                 rand_state=True):
         self.dataset = chainer.datasets.ImageDataset(dataset)
         self.num_class = num_class
-        self.num_per_class = num_per_class
+        self.num_data_per_class = num_data_per_class
         self.num_sample_class = num_sample_class
         self.num_sample_per_class = num_sample_per_class
-        self.order = None
+        self.rand_state = rand_state
+
+        self.sampleddataset = None
+        self.resampling()
 
     def __call__(self):
-        if self.order is None:
-            self.resampling()
-        return self.order[:self.num_sample_class]
+        return self.sampleddataset
+
+    def __len__(self):
+        return len(self.sampleddataset)
 
     def resampling(self):
-        self.order = np.random.permutation(self.num_class)
+        class_indices = random.sample(range(self.num_class),
+                                      self.num_sample_class) * self.num_sample_per_class
+        per_class_indices = np.asarray([
+            random.sample(range(self.num_data_per_class), self.num_sample_per_class)
+            for _ in range(self.num_sample_class)
+        ]).T
+        labels = list(range(self.num_sample_class)) * self.num_data_per_class
+        data_indices = np.asarray(class_indices) * self.num_data_per_class + per_class_indices.flat
+        if self.rand_state:
+            order = np.random.permutation(self.num_sample_class * self.num_sample_per_class)
+        else:
+            order = np.arange(self.num_sample_class * self.num_sample_per_class)
+        print(order)
+        self.sampleddataset = list(
+            zip(self.dataset[data_indices[order]], np.asarray(labels)[order]))
+
+    def get_example(self, i):
+        return self.sampleddataset[i]
 
 
-def make_sampling_dataset_for_omniglot_traindata(basedir):
+def make_sampling_dataset_for_omniglot_traindata(basedir,
+                                                 num_sample_class=5,
+                                                 num_sample_per_class=10):
     paths = glob.glob(os.path.join(basedir, "*", "*", "*"))
     num_class = len(glob.glob(os.path.join(basedir, "*", "*")))
 
-    return SamplingDataset(paths, num_class, 20, 5, 10)
+    return SamplingDataset(paths, num_class, 20, num_sample_class, num_sample_per_class)
 
 
 class RandomSampleIterator(chainer.dataset.Iterator):
@@ -94,10 +122,5 @@ if __name__ == "__main__":
     parser.add_argument('--basedir', '-d', default="../data/omniglot/images_background", help='')
     args = parser.parse_args()
 
-    d1 = "a b c d e f g".split(" ")
-    d2 = "1 2 3 4 5 6 7".split(" ")
-    ds = chainer.datasets.TupleDataset(d1, d2)
-    rsi = RandomSampleIterator(ds, 1, 3, 1)
-    print(rsi.next())
     sd = make_sampling_dataset_for_omniglot_traindata(args.basedir)
     print(sd())
